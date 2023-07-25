a62cb79d2a826a31b650f6cb92df1c10ac5ed911
Yaseen Zafar <yaseen.zafar@xgrid.co>
2023-07-25 15:03:23 +0500

    Title: Integration Testing through pipeline Link: #63
       [PS-4]
         - Addressed some feedbacks
    
       [PS-3]
         - Tested it through pipeline
    
       [PS-2]
         - Write all integration tests
    
       [PS-1]
         - Resolved the lambda testing issue.
    
    Signed-off-by: Yaseen Zafar <yaseen.zafar@xgrid.co>

--- infrastructure/modules/serverless/data.tf
+++ infrastructure/modules/serverless/data.tf
@@ -12,13 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-// Terraform Remote State
-
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-xc3-test1" // S3 bucket for terraform state management
-    key            = "test/test.tfstate"        // S3 object key for terraform state file to maintain history of resources
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-lock"
-  }
+# tflint-ignore: terraform_unused_declarations
+data "aws_kms_alias" "check_existing_kms" {
+  name = "alias/${var.namespace}-kms-key"
 }
